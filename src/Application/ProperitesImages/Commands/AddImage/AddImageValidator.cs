e5a5814dc0b0c2ac1843178c5811092831a990da
Fernando Marquez <fernandobambam@outlook.com>
2023-10-21 14:19:14 -0500

    Validations are added to add images

---
+++ src/Application/ProperitesImages/Commands/AddImage/AddImageValidator.cs
@@ -0,0 +1,54 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.ProperitesImages.Commands.AddImage
+{
+    public class AddImageValidator : AbstractValidator<AddImageRequest>
+    {
+        public AddImageValidator()
+        {
+            RuleFor(x => x).Custom((model, context) =>
+            {
+                bool failed = false;
+
+                if (FileIsEmpty(model.File))
+                {
+                    context.AddFailure("File is empty");
+                    failed = true;
+                }
+
+                if (failed)
+                    return;
+
+                if (ValidateFileExtension(model.File))
+                {
+                    context.AddFailure("Invalid Format");
+                    failed = true;
+                }
+            });
+        }
+
+        private bool FileIsEmpty(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool ValidateFileExtension(IFormFile file)
+        {
+            string[] allowedImageExtensions = { ".jpg", ".jpeg", ".png"};
+
+            string fileExtension = Path.GetExtension(file.FileName).ToLower();
+            if (!allowedImageExtensions.Contains(fileExtension))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
